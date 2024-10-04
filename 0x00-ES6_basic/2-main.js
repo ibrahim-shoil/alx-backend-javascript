a4554d0b22d811f944efe2026c5fe2b767d3d45b
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-10-04 23:19:59 +0300

    Third

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,7 @@
+// 2-main.js
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);  // Expected Output: [ 'SOMA', 'Union Square', 'Noe Valley' ]
+
