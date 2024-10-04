c55c679166208c44e89e8cc2566470561ad2ef01
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-10-04 22:22:15 +0300

    second

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    // The variables here are not needed as they do not affect the output
-    // So, we can safely remove these lines.
-    // let task = true;
-    // let task2 = false;
+    /*
+      const task = true;
+      const task2 = false;
+      */
   }
 
   return [task, task2];
